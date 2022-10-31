901d7cce0b203f46e8f22ee453efafb678c5ea89
AmiableShob <shoubhit2090@gmail.com>
2022-10-31 13:36:39 +0530

    Shifting to CustomTkinter

--- gui.py
+++ gui.py
@@ -1,44 +1,36 @@
 from tkinter import *
 from tkinter import ttk
+from customtkinter import *
 from numpy import *
-from PIL import Image, ImageTk, ImageOps
 import warnings
 
 warnings.filterwarnings("ignore")
 
-root = Tk( )
+root = CTk( )
 root.title("Wordle Solver")
+root.geometry(f"{200}x{200}")
+set_appearance_mode("System")
+set_default_color_theme("green")
 
 fobj = open("five-letter-words.txt", "r")
 
-# Adding a background
-back = Image.open("background.png")
-back = back.transpose(Image.FLIP_LEFT_RIGHT)
-background = ImageTk.PhotoImage(back)
-
-# Start button
-img = ImageOps.expand(Image.open("start.png"), border=8, fill="black")
-img = img.resize((200,100))
-start_img = ImageTk.PhotoImage(img)
-
 # Initializing the frames
-hide_frame = Frame(root)
-start_frame = Frame(root)
-eliminate_frame = Frame(root)
-loading_frame = Frame(root)
-green_tile_frame = Frame(root)
-yellow_tile_frame = Frame(root)
-output_frame = Frame(root)
+hide_frame = CTkFrame(root)
+start_frame = CTkFrame(root)
+eliminate_frame = CTkFrame(root)
+loading_frame = CTkFrame(root)
+green_tile_frame = CTkFrame(root)
+yellow_tile_frame = CTkFrame(root)
+output_frame = CTkFrame(root)
 
 # Everything in the start frame
 def start(frm):
 
 	frm.grid_forget( )
 
-	back_l = Label(start_frame, image=background)
-	back_l.place(x=0, y=0)
-	start = Button(master=start_frame, image=start_img, borderwidth=-10, command=lambda: eliminate(start_frame))
-	start.grid(row=50, column=50, padx=100, pady=75)
+	start = CTkButton(master=start_frame, text="Start", borderwidth=0, command=lambda: eliminate(start_frame)) # image=start_img, 
+	
+	start.grid(row=0, column=0)
 	start_frame.grid(row=1,column=1)
 
 #Everything in the eliminate frame
@@ -46,9 +38,11 @@ def eliminate(frm):
 
 	frm.grid_forget( )
 
-	enter = Label(master=eliminate_frame, text="Enter the letters that are NOT there")
-	comment = Label(master=eliminate_frame, text="[No Commas Required]")
-	letters_not_there = ttk.Entry(master=eliminate_frame, width=26)
+	enter = CTkLabel(master=eliminate_frame, text="Enter the letters that are NOT there")
+	comment = CTkLabel(master=eliminate_frame, text="[No Commas Required]")
+
+	letters_not_there = CTkEntry(master=eliminate_frame, placeholder_text="Enter:", width=1)
+	
 	enter.grid(row=0,column=0)
 	comment.grid(row=1,column=0)
 	letters_not_there.grid(row=2,column=0)
