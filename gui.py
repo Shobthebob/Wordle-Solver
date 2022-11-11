96d5c308cdd1f01a64736be74284662bc2fd0269
AmiableShob <shoubhit2090@gmail.com>
2022-11-12 03:49:38 +0530

    finished the front end for green_tile_frame

--- gui.py
+++ gui.py
@@ -41,6 +41,7 @@ def hide_show(curr_frame):
 	else:
 		back.grid(row=1, column=0, ipadx=10)
 		nextt.grid(row=1, column=1, ipadx=10)
+	return
 
 # Everything in the start frame (main frame)
 def start(frm):
@@ -88,15 +89,103 @@ def eliminate(frm):
 # Everything in the frame that asks for green tiled chars
 def greenTile(frm):
 
-	def check(ch):
-		
-		ch = ch.get( )
-		if((ch=="") or (ch.isalpha( ))) and (len(ch)<2):
-			nextt.configure(state="normal", justify=CENTER)
-		else:
-			nextt.configure(state="disabled")
-			comment.configure(text="[ONLY 1 CHAR/BLANKSPACE PER ENTRY]")			
+	# creating different functions to make sure no one gives a wrong input in each entry
+	def check1(cha1, index, mode):
 
+		cha1 = ch1.get( )
+		# for 1st entry
+		if((not cha1.isalpha( ) and cha1!="") or (len(cha1)>1)):
+			nextt.configure(state="disabled")
+			c2.configure(state="disabled")
+			c3.configure(state="disabled")
+			c4.configure(state="disabled")
+			c5.configure(state="disabled")
+			comment.configure(text="[ONLY 1 ALPHABET/BLANKSPACE PER ENTRY]")
+		else:
+			nextt.configure(state="normal")
+			c2.configure(state="normal")
+			c3.configure(state="normal")
+			c4.configure(state="normal")
+			c5.configure(state="normal")
+			comment.configure(text="Valid")				
+		return
+	def check2(cha2, index, mode):
+
+		cha2 = ch2.get( )
+		# for 2nd entry
+		if((not cha2.isalpha( ) and cha2!="") or (len(cha2)>1)):
+			nextt.configure(state="disabled")
+			c1.configure(state="disabled")
+			c3.configure(state="disabled")
+			c4.configure(state="disabled")
+			c5.configure(state="disabled")
+			comment.configure(text="[ONLY 1 ALPHABET/BLANKSPACE PER ENTRY]")
+		else:
+			nextt.configure(state="normal")
+			c1.configure(state="normal")
+			c3.configure(state="normal")
+			c4.configure(state="normal")
+			c5.configure(state="normal")
+			comment.configure(text="Valid")	
+		return
+	def check3(cha3, index, mode):
+
+		cha3 = ch3.get( )
+		# for 3rd entry
+		if((not cha3.isalpha( ) and cha3!="") or (len(cha3)>1)):
+			nextt.configure(state="disabled")
+			c1.configure(state="disabled")
+			c2.configure(state="disabled")
+			c4.configure(state="disabled")
+			c5.configure(state="disabled")
+			comment.configure(text="[ONLY 1 ALPHABET/BLANKSPACE PER ENTRY]")
+		else:
+			nextt.configure(state="normal")
+			c1.configure(state="normal")
+			c3.configure(state="normal")
+			c4.configure(state="normal")
+			c5.configure(state="normal")
+			comment.configure(text="Valid")
+		return
+	def check4(cha4, index, mode):
+
+		cha4 = ch4.get( )
+		# for 4th entry 
+		if((not cha4.isalpha( ) and cha4!="") or (len(cha4)>1)):
+			nextt.configure(state="disabled")
+			c1.configure(state="disabled")
+			c2.configure(state="disabled")
+			c3.configure(state="disabled")
+			c5.configure(state="disabled")
+			comment.configure(text="[ONLY 1 ALPHABET/BLANKSPACE PER ENTRY]")
+		else:
+			nextt.configure(state="normal")
+			c1.configure(state="normal")
+			c2.configure(state="normal")
+			c3.configure(state="normal")
+			c5.configure(state="normal")
+			comment.configure(text="Valid")	
+		return
+	def check5(cha5, index, mode):
+
+		cha5 = ch5.get( )
+		#for 5th entry
+		if((not cha5.isalpha( ) and cha5!="") or (len(cha5)>1)):
+			nextt.configure(state="disabled")
+			c1.configure(state="disabled")
+			c2.configure(state="disabled")
+			c3.configure(state="disabled")
+			c4.configure(state="disabled")
+			comment.configure(text="[ONLY 1 ALPHABET/BLANKSPACE PER ENTRY]")
+		else:
+			nextt.configure(state="normal")
+			c1.configure(state="normal")
+			c2.configure(state="normal")
+			c3.configure(state="normal")
+			c4.configure(state="normal")
+			comment.configure(text="Valid")	
+		return
+	
 	hide_show("green_tile_frame")
 	frm.grid_forget( )
 	root.geometry(f"{289}x{140}")
@@ -106,17 +195,18 @@ def greenTile(frm):
 	enter2 = CTkLabel(master=green_tile_frame, text="Leave the rest blank")
 	comment = CTkLabel(master=green_tile_frame, text="[1 Block = 1 Char]")
 
+	# The values that we input in the 5 entry boxes
 	ch1 = StringVar( )
 	ch2 = StringVar( )
 	ch3 = StringVar( )
 	ch4 = StringVar( )
 	ch5 = StringVar( )
-	ch1.trace_add("write", lambda: check(ch1))
-	ch2.trace_add("write", lambda: check(ch2))
-	ch2.trace_add("write", lambda: check(ch3))
-	ch3.trace_add("write", lambda: check(ch4))
-	ch4.trace_add("write", lambda: check(ch5))
-
+	ch1.trace_variable("w", check1)
+	ch2.trace_variable("w", check2)
+	ch3.trace_variable("w", check3)
+	ch4.trace_variable("w", check4)
+	ch5.trace_variable("w", check5)
+	
 	# The 5 entry boxes 
 	c1 = CTkEntry(master=green_tile_frame, width=25, justify=CENTER, textvariable=ch1)
 	c2 = CTkEntry(master=green_tile_frame, width=25, justify=CENTER, textvariable=ch2)
@@ -128,13 +218,20 @@ def greenTile(frm):
 	enter1.grid(row=0, column=0, columnspan=5)
 	enter2.grid(row=1, column=0, columnspan=5)
 	comment.grid(row=2, column=0, columnspan=5)
-	c1.grid(row=3,column=0,columnspan=4)
-	c2.grid(row=3,column=1,columnspan=3)
-	c3.grid(row=3,column=2,columnspan=2)
-	c4.grid(row=3,column=3,columnspan=1)
-	c5.grid(row=3,column=4,columnspan=1)
+	c1.grid(row=3,column=0,ipadx=3,sticky="ew")
+	c2.grid(row=3,column=1,ipadx=3,sticky="ew")
+	c3.grid(row=3,column=2,ipadx=3,sticky="ew")
+	c4.grid(row=3,column=3,ipadx=3,sticky="ew")
+	c5.grid(row=3,column=4,ipadx=3,sticky="ew")
 	green_tile_frame.grid(row=0, column=0, columnspan=3)
 	return
+
+# Everything in the yellow tile frame
+# def yellowTile(frm):
+	
+# 	hide_show("yellowTile")
+# 	frm.grid_forget( )
+
 
 start(hide_frame)
 
